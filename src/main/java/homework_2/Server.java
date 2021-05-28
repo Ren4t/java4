70a65e6dfcf0c22dd322b12beb158084bdcaf511
Ren4t <ren4tab@yandex.ru>
2021-05-28 19:01:25 +0300

    homework_2 chapter2 done

--- src/main/java/homework_2/Server.java
+++ src/main/java/homework_2/Server.java
@@ -2,21 +2,26 @@ package homework_2;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
-import java.nio.Buffer;
 import java.nio.ByteBuffer;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Iterator;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
+
 
 public class Server {
     private ByteBuffer buffer;
     private ServerSocketChannel serverSocketChannel;
     private Selector selector;
+    private String dir = "server";
 
     public Server() throws Exception {
         buffer = ByteBuffer.allocate(100);
@@ -66,14 +71,48 @@ public class Server {
             buffer.clear();
         }
 
-        String message = sb.toString();
+        String message = sb.toString().trim();
 
-        channel.write(ByteBuffer.wrap(message.getBytes(UTF_8)));
+        if(message.equals("ls")) {
+            String files = Files.list(Paths.get(dir))
+                    .map(f -> f.getFileName().toString())
+                    .collect(Collectors.joining("\n\r"));
+            channel.write(ByteBuffer.wrap(files.getBytes(UTF_8)));
+        }else if (message.startsWith("cat")) {
+            String fileName = message.replace("cat ","");
+            byte[] bytes = Files.readAllBytes(Paths.get(dir,fileName));
+            channel.write(ByteBuffer.wrap(bytes));
+        } else if(message.startsWith("touch")) {
+            try {
+                String[] str = message.split(" ",0);
+                Path path =Paths.get(dir,str[1]);
+                if (!Files.exists(path)) {
+                        Files.createDirectories(path.getParent());
+                        Files.createFile(path);
+
+                }
+            } catch (Exception e){
+                System.err.println(e);
+            }
+        } else if(message.startsWith("mkdir")) {
+            String[] str = message.split(" ",0);
+            Path path = Paths.get(dir, str[1]);
+            if (!Files.exists(path)) {
+                try {
+                    Files.createDirectory(path);
+                } catch (IOException e) {
+                    System.err.println(e);
+                }
+            }
+        }
+
+        channel.write(ByteBuffer.wrap(("\n\r").getBytes(UTF_8)));
     }
 
     private void handleAccept(SelectionKey key) throws IOException {
         SocketChannel channel = serverSocketChannel.accept();
         channel.configureBlocking(false);
+        channel.write(ByteBuffer.wrap("Welcome to server".getBytes(UTF_8)));
         channel.register(selector, SelectionKey.OP_READ,"Hello");
-    }
+}
 }
